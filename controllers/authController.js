41b5ee34ece896a33de9bc70024beb18eae7728c
Spandan Pankaj Marathe <spandanmarathe95@gmail.com>
2024-07-13 22:00:49 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,7 +79,7 @@ const loginUser = async (req, res) => {
 
 const getProfile = async (req, res) => {
   try {
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
